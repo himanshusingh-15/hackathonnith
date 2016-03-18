87e8504a5025ffbfb071f6b685113361b911b665
Himanshu Singh <himanshusingh.singh06@gmail.com>
2016-03-18 17:45:23 +0530

    Acquired the form entry to process

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
+	var name = req.body.name;
+	var rollno = req.body.rollno;
+	var emailid = req.body.emailid;
+	var phoneno = req.body.phoneno;
+	var languageinterested = req.body.languageinterested;
+	var projectidea = req.body.projectidea;
+	var suggestions = req.body.suggestions;
   res.render('index', { title: 'Express' });
 });
 
