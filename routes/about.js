7d9a7b30f958b51a18b7f30ab4b10b4ac79f4542
sahilbadyal <sbadyals@gmail.com>
2016-03-20 23:50:57 +0530

    done check

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('about', { title: 'About' });
+  res.render('/views/about.html');
 });
 
 module.exports = router;
