066e45ba6853da025d5b65b9aef5efb37fa38938
sahilbadyal <sbadyals@gmail.com>
2016-03-20 22:15:39 +0530

    done check

--- routes/index.js
+++ routes/index.js
@@ -30,6 +30,7 @@ router.post('/', function(req, res, next) {
 				check=true;
 			}
 		});
+		console.log(check);
 		if(check!=true){
 			client.query('INSERT INTO entry(rollno,name,emailid,phoneno,language,idea,suggestions) VALUES($1,$2,$3,$4,$5,$6,$7)', [rollno,name,emailid,phoneno,languageinterested,projectidea,suggestions]);
 		}
